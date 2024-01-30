7897ef6ff924cca9b630c498d3838c48d671bcd9
avasilenko <snakevil@mail.ru>
2024-01-30 12:49:50 +0300

    modify task3_1

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,13 +3,13 @@
 class Program
 {
     static void Main(string[] args)
-{   // Создаем рандомную строку
+{   
     string rndText = "шалаш";
     char[] reverseText = rndText.ToCharArray();
-    // Используем функцию для разворота строки
+  
     Array.Reverse(reverseText);
     string finalText = new string(reverseText);
-    // Сравниваем начальную строку и реверсированную с помощью условной конструкции if/else и выводим на экран ответ
+    
     if (rndText == finalText)
     {
         Console.WriteLine("Данная запись является палиндромом");
