162265f4b8a063217777805fa1bc416ea448720e
avasilenko <snakevil@mail.ru>
2024-01-30 12:39:12 +0300

    modify task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+{   // Создаем рандомную строку
+    string rndText = "МадаМ";
+    char[] reverseText = rndText.ToCharArray();
+    // Используем функцию для разворота строки
+    Array.Reverse(reverseText);
+    string finalText = new string(reverseText);
+    // Сравниваем начальную строку и реверсированную с помощью условной конструкции if/else и выводим на экран ответ
+    if (rndText == finalText)
+    {
+        Console.WriteLine("Данная запись является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Данная запись не является палиндромом");
+    }
+}
+}
