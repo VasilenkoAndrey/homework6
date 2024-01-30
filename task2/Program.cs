2a05417717177b935ae72954d288820871e21d5b
avasilenko <snakevil@mail.ru>
2024-01-30 12:49:45 +0300

    modify task2_1

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,9 +3,8 @@
 class Program
 {
     static void Main(string[] args)
-    {   // Создаем строку содержащую буквы разного регистра
+    {   
         string stringOfLetters = "WoRdS";
-        // Изменяем регистр букв в строке
         string result = stringOfLetters.ToLower();
         Console.WriteLine(result);
     }
