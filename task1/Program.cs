34e160d93606bcc59b41857193d873fe703893c3
avasilenko <snakevil@mail.ru>
2024-01-30 12:49:39 +0300

    modify task1_1

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,15 +4,13 @@ class Program
 { 
     static void Main(string[] args) 
     { 
-        // Задаем двумерный массив символов 4х3   
+        
         char[,] rndArray = new char[4, 4]{ 
-        {'$', '6', '1', '2'}, 
-        { '5', '#', '!', '7'}, 
-        { 'k', '%', '&', '4'}, 
-        { '8', '*', '@', '9'}}; 
-        // Создаем пустую строку 
+        {'$', 'h', 'e', 'l'}, 
+        { '_', 'l', '0', 'w'}, 
+        { '0', 'r', '!', 'd'}, 
+        { '2', '*', '2', '4'}}; 
         string newString = " "; 
-        // Заполняем строку символами из массива 
         for (int i = 0; i < rndArray.GetLength(0); i++) 
         { 
             for (int j = 0; j < rndArray.GetLength(1); j++) 
